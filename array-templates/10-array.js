58801a2adaa0fcafe8922387d508af9cee49920e
realdiegor <diegoreyes150103@gmail.com>
2023-06-20 15:37:55 -0400

    refactor: CJS to ESM

--- array-templates/10-array.js
+++ array-templates/10-array.js
@@ -1,3 +1,3 @@
 const ARRAY = [2, 72, 5, 112, 6, 1, 40, 218, 100, 90];
 
-module.exports = ARRAY
+export default ARRAY
--- quick-sort/index.js
+++ quick-sort/index.js
@@ -1,13 +1,18 @@
-const ARRAY = require('../array-templates/10-array');
+import chalk from 'chalk';
+import ARRAY from '../array-templates/100-array.js';
 
-function getPartition (array, low, high) {
-  console.log(`Pivot is: ${array[high]}`)
+function partition (array, low, high) {
+  console.log(chalk.magenta.bold(`Pivot is: ${array[high]}`))
   let i = low - 1;
   let j = low;
 
   while ( j < high) {
     if ( array[j] < array[high] ) {
       i++;
+      if (i === j) {
+        j++;
+        continue;
+      }
       console.log(`Moving ${array[i]} and ${array[j]} ...`);
       [ array[i], array[j] ] = [ array[j], array[i] ];
     }
@@ -18,7 +23,7 @@ function getPartition (array, low, high) {
   console.log(`Before moving pivot: ${array}`);
   [ array[i + 1], array[high] ] = [ array[high], array[i + 1] ]
 
-  console.log(`Sorted array is: ${array} \n`)
+  console.error(chalk.green.bold(`Sorted array is: ${array.join(', ').replace(`${array[i + 1]}`, chalk.red.bold(`${array[i + 1]}`))} \n`))
 
   return i + 1;
 }
@@ -26,8 +31,8 @@ function getPartition (array, low, high) {
 function quickSort (array, low, high) {
 
   if (low < high) {
-    const pi = getPartition(array, low, high);
-    console.log(`Partition point is: ${pi}`)
+    const pi = partition(array, low, high);
+    console.log(`Partition index is: ${pi}`)
     quickSort(array, low, pi -1);
     quickSort(array, pi + 1, high);
   }
@@ -37,4 +42,4 @@ console.time('sort')
 quickSort(ARRAY, 0, ARRAY.length - 1)
 console.timeEnd('sort')
 
-console.log(ARRAY)
+console.log(chalk.blue(ARRAY))
